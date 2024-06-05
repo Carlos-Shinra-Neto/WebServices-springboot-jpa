42b6f050d7862caee85109bf28a826cf9cf26f65
Carlos <leosergi09@gmail.com>
2024-06-05 15:22:05 -0300

    ProjectWebService, > User.java -> Created user class, > UserResorces.java -> Created UserResources class and method findAll.

---
+++ src/main/java/com/webservice/project/resources/UserResources.java
@@ -0,0 +1,19 @@
+package com.webservice.project.resources;
+
+import com.webservice.project.entities.User;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping(value = "/users")
+public class UserResources {
+
+    //TODO Adcionar REGEX para telefone, email
+    @GetMapping
+    public ResponseEntity<User> findAll(){
+        User u = new User(1L, "Carlos", "carlos@email.com", "993003-7373", "12345");
+        return ResponseEntity.ok().body(u);
+    }
+}
